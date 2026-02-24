9e95cf3c105c2900a46822d078e7ce3a3b5a9d1b
Hardip <hardip01@gmail.com>
2026-02-24 14:28:49 +0000

    EDIT: Added extra hello message for David

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Hardip"));
+        System.out.println(sayHelloToSomeone("David"));
         System.out.println(sayGoodbye());
     }
 
